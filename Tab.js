c44f6a9122af01d465755bdf60aed6b29b6e05a2
jhuangpku <huangjing@cq01-rdqa-dev057.cq01.baidu.com>
2014-12-12 07:53:22 +0800

    update

--- Tab.js
+++ Tab.js
@@ -1,31 +1,27 @@
 function Tab(options) {
     // 实现构造函数
-    array = []
-    tabs = options['main'].getElementsByTagName('li')
-    activeIndex = options['activeIndex']
-    for (var i = 0, l = tabs.length; i < l; i++)
-    {
-        array.push(tabs[i].dataset.tabItem)
-    } 
+    //this.array = []
+    this.tabs = options['main'].getElementsByTagName('li')
+    activeIndex = options['activeIndex'] 
     this.setActiveIndex(activeIndex)
 }
 
 Tab.prototype.setActiveIndex = function (activeIndex) {
     // 实现setActiveIndex方法
-    tabs = document.getElementById('myTab').getElementsByTagName('li')
-    for(var i = 0, l = array.length; i < l; i++)
+    for(var i = 0, l = this.tabs.length; i < l; i++)
     {
         if (i == activeIndex)
         {
-            document.getElementById(array[i]).style.display = 'block' 
-            tabs[i].style.background = '#c2c2c2'
-            tabs[i].style.color = 'white'
+
+            document.getElementById(this.tabs[i].dataset.tabItem).style.display = 'block' 
+            this.tabs[i].style.background = '#c2c2c2'
+            this.tabs[i].style.color = 'white'
         }
         else
         {
-            document.getElementById(array[i]).style.display = 'none'
-            tabs[i].style.background = 'white'
-            tabs[i].style.color = 'black'   
+            document.getElementById(this.tabs[i].dataset.tabItem).style.display = 'none' 
+            this.tabs[i].style.background = 'white'
+            this.tabs[i].style.color = 'black'   
         }
     }
 };
